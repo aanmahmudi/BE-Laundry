2b282f211971af3ae4ae59dc767b3838e3c6fd7c
Aan <aanmahmudi88@gmail.com>
2024-11-19 16:51:59 +0700

    ada perubahan pada entity dan service

--- BE-Laundry/src/main/java/com/laundry/BE_Laundry/Entity/Customer.java
+++ BE-Laundry/src/main/java/com/laundry/BE_Laundry/Entity/Customer.java
@@ -1,6 +1,9 @@
 package com.laundry.BE_Laundry.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -8,7 +11,9 @@ import lombok.Data;
 @Entity
 @Table(name = "customers")
 public class Customer {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private Long id;
 	
 	private String name;
--- BE-Laundry/src/main/java/com/laundry/BE_Laundry/Service/TransactionService.java
+++ BE-Laundry/src/main/java/com/laundry/BE_Laundry/Service/TransactionService.java
@@ -2,6 +2,7 @@ package com.laundry.BE_Laundry.Service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.laundry.BE_Laundry.Entity.Transaction;
@@ -10,6 +11,7 @@ import com.laundry.BE_Laundry.Repository.TransactionRepository;
 @Service
 public class TransactionService {
 	
+	@Autowired
 	private TransactionRepository transactionRepository;
 	
 	public Transaction createTransaction(Transaction transaction) {
