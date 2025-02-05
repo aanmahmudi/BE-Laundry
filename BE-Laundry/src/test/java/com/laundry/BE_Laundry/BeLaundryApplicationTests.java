2433378c80f11ce13fd0d9c458f158e2f1f291a2
hendisantika <hendisantika@yahoo.co.id>
2025-02-05 11:39:08 +0700

    #4 | Hendi | feat: Setup Test Containers 🫸🌀✏️📗🐧🐳⬆

--- BE-Laundry/src/test/java/com/laundry/BE_Laundry/BeLaundryApplicationTests.java
+++ BE-Laundry/src/test/java/com/laundry/BE_Laundry/BeLaundryApplicationTests.java
@@ -2,12 +2,25 @@ package com.laundry.BE_Laundry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BeLaundryApplicationTests {
 
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine3.21");
+
 	@Test
-	void contextLoads() {
+	void connectionEstablished() {
+		assertThat(postgres.isCreated()).isTrue();
+		assertThat(postgres.isRunning()).isTrue();
 	}
 
 }
